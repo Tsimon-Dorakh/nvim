9fa659a06c06780e988290479a305957336db304
Tsimon Dorakh <t1myrkq@gmail.com>
2024-09-23 19:04:36 +0300

    Up

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,33 +1,45 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
+
+-- don't add this function in the `lsp_attach` callback.
+-- `format_on_save` should run only once, before the language servers are active.
+lsp_zero.format_on_save({
+    format_opts = {
+        async = false,
+        timeout_ms = 10000,
+    },
+    servers = {
+        ['rust_analyzer'] = { 'rust' },
+    }
+})
 
 -- here you can setup the language servers
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  -- Replace the language servers listed here
-  -- with the ones you want to install
-  ensure_installed = {'lua_ls', 'rust_analyzer', 'clangd'},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  }
+    -- Replace the language servers listed here
+    -- with the ones you want to install
+    ensure_installed = { 'lua_ls', 'rust_analyzer', 'clangd' },
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+    }
 })
 
 require('lspconfig').lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = {'vim'}
-      }
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
     }
-  }
 })
 
 local cmp = require('cmp')
@@ -39,8 +51,7 @@ cmp.setup({
         autocomplete = false, -- Disable showing autocomplete popup by default
     },
     mapping = cmp.mapping.preset.insert({
-        ['<CR>'] = cmp.mapping.confirm({select = false}), -- Confirm popup item by Enter
-        ['<C-Space>'] = cmp.mapping.complete(), -- Show autocomplete Ctrl+Space
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Confirm popup item by Enter
+        ['<C-Space>'] = cmp.mapping.complete(),             -- Show autocomplete Ctrl+Space
     }),
 })
-
