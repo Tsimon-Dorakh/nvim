1a730389bf66c5bfc6240a9d899bcca1b060c8dd
Tsimon Dorakh <t1myrkq@gmail.com>
2025-05-12 13:45:02 +0300

    Up

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,17 @@
 local telescope = require('telescope')
 
 telescope.setup {
-    defaults = { file_ignore_patterns = {
-        "node_modules",
-        "target"
-    } }
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules",
+            "target"
+        },
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+        },
+    },
 }
 
 telescope.load_extension("live_grep_args")
@@ -13,5 +20,15 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
-
+vim.keymap.set('n', '<leader>ps', function()
+    telescope.extensions.live_grep_args.live_grep_args({ vimgrep_arguments = {
+        "rg",
+        "--hidden",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+    } })
+end)
