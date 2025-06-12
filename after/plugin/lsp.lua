113f2a29250abb11c7a7fbf0d1f15a13e26fafac
Tsimon Dorakh <t1myrkq@gmail.com>
2025-06-12 13:53:55 +0300

    Up

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -24,7 +24,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
     -- Replace the language servers listed here
     -- with the ones you want to install
-    ensure_installed = { 'lua_ls', 'rust_analyzer', 'clangd' },
+    ensure_installed = { 'lua_ls', 'rust_analyzer', 'clangd', 'gopls' },
     handlers = {
         function(server_name)
             require('lspconfig')[server_name].setup({})
